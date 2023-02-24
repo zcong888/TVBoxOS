0ea2a2699a5ce905466403ae57b34aa9018ff898
zcong2008 <44361549+zcong2008@users.noreply.github.com>
2023-02-24 13:12:38 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -274,29 +274,7 @@ public class ModelSettingFragment extends BaseLazyFragment {
                 dialog.show();
             }
         });
-        findViewById(R.id.llApi).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FastClickCheckUtil.check(v);
-                ApiDialog dialog = new ApiDialog(mActivity);
-                EventBus.getDefault().register(dialog);
-                dialog.setOnListener(new ApiDialog.OnListener() {
-                    @Override
-                    public void onchange(String api) {
-                        Hawk.put(HawkConfig.API_URL, api);
-                        tvApi.setText(api);
-                    }
-                });
-                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        ((BaseActivity) mActivity).hideSysBar();
-                        EventBus.getDefault().unregister(dialog);
-                    }
-                });
-                dialog.show();
-            }
-        });
+
 
 
 
